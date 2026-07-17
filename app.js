58ad6b290bc929110fef7a8c2f517a01ba1fea90
EZPlanningStudio <daniellanechita@gmail.com>
2026-07-17 18:36:20 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -5266,6 +5266,11 @@ async function importLatestBackup() {
 }
 
 async function smartImportBackup() {
+    if (!window.showDirectoryPicker) {
+        document.getElementById("importJson").click();
+        return;
+    }
+
     const directoryHandle = await getBackupDirectoryHandle();
 
     if (directoryHandle) {
